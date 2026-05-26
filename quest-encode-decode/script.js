7379b5d362ae472dd60786c73217b37e8c9a75f3
solas-krasovskykh <solas.krasovskykh@gmail.com>
2026-05-26 23:15:30 +0200

    link in encode-decode fixed

--- quest-encode-decode/script.js
+++ quest-encode-decode/script.js
@@ -6,7 +6,7 @@ function checkPassword()
 
     if(password.value.toLowerCase() == "science") {
         resultBox.innerHTML = "Ready for the next one?";
-        resultBox.href = "https://solas-krasovskykh.github.io/search-quest/archive/quest-archive/quest-archive.html"
+        resultBox.href = "https://solas-krasovskykh.github.io/search-quest/quest-archive/quest-archive.html"
     }
     else {
         resultBox.innerHTML = "hmm, not quite there, try again";
